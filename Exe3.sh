735fd5bd1b982caedd9dd143f0aa5e264df06579
Paulo Ascenção <frozsyn@gmail.com>
2017-11-08 20:29:47 +0000

    Update Exe3.sh

--- Exe3.sh
+++ Exe3.sh
@@ -3,7 +3,7 @@ mkdir -p Grupo_1/exer1
 cd Grupo_1/exer1
 touch fich1.txt fich2.txt fich3.txt fich4.dat fich5.dat fich6.dat fich7.doc fich8.doc fich9.doc
 cd
-mkdir -p Grupo_1/desktop/fichs_txt Grupo_1/desktop/fichs_dat Grupo_1/desktop/fichs_dot
+mkdir -p Grupo_1/desktop/fichs_txt Grupo_1/desktop/fichs_dat Grupo_1/desktop/fichs_doc
 mv Grupo_1/exer1/*.txt ~/Grupo_1/desktop/fichs_txt
 mv Grupo_1/exer1/*.dat ~/Grupo_1/desktop/fichs_dat
 mv Grupo_1/exer1/*.doc ~/Grupo_1/desktop/fichs_doc
